239f5f663fa55d1bd78081e7c43d63d2a8983948
igafarov <igafarov@dtln.ru>
2024-01-08 18:41:52 +0400

    refactor locator

--- src/test/java/pages/components/DatePickerComponent.java
+++ src/test/java/pages/components/DatePickerComponent.java
@@ -9,7 +9,7 @@ public class DatePickerComponent {
 
     private final SelenideElement yearOfBirth = $(".react-datepicker__year-select"),
             monthOfBirth = $(".react-datepicker__month-select"),
-            birthday = $(".react-datepicker__month:not(.react-datepicker__day--outside-month)");
+            birthday = $(".react-datepicker__day.react-datepicker__day--0\"+day+\":not(.react-datepicker__day--outside-month))");
 
     public void setDateOfBirth(String year, String month, int day) {
         yearOfBirth.selectOption(year);
